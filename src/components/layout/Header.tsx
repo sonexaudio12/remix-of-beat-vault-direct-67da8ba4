ee71b139d54362879ebd09ecbaf90e9666a9a72c
sonexaudio12 <carmencataro1@gmail.com>
2026-01-19 12:57:50 -0500

    Update Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,7 +3,7 @@ import { Button } from '@/components/ui/button';
 import { useCart } from '@/hooks/useCart';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import logo from '@/assets/logo-hd.png';
+import logo from 'src/assets/logo-real.png';
 
 export function Header() {
   const { itemCount } = useCart();
